c4bb47128fcb80ef33602b53b5716fe1dfc6ebb7
UTKARSH MANI TRIPATHI <UTKARSH MANI TRIPATHI>
2017-03-25 09:48:33 +0530

    fresh_add

--- web/firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.6/firebase-messaging.js');
-
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-  messagingSenderId: '670781077200'
-});
-var messaging = firebase.messaging();
